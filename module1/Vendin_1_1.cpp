9402637d3a307c08f27967a72a158745598767ae
Vendin <v.a.5@list.ru>
2014-11-30 01:31:05 +0300

    mod1

---
+++ module1/Vendin_1_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+int N, i = 2, A, B;
+scanf("%d", &N);
+while (N % i) {
+i++;
+}
+if (N % i) {
+i = 1;
+}
+A = N / i;
+B = N - A;
+printf("%d %d", A, B);
+return 0;
+}
