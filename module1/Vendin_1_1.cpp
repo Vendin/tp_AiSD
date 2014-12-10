c414e3f792ebabfd59837fc4fb30a6ab6dd49d65
Vendin <v.a.5@list.ru>
2014-12-10 19:27:40 +0300

    Update

--- module1/Vendin_1_1.cpp
+++ module1/Vendin_1_1.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-int N, i = 2, A, B;
-scanf("%d", &N);
-while (N % i) {
-i++;
-}
-if (N % i) {
-i = 1;
-}
-A = N / i;
-B = N - A;
-printf("%d %d", A, B);
-return 0;
+  int N, i = 2, A, B;
+  scanf("%d", &N);
+  while (N % i) {
+    i++;
+  }
+  if (N % i) {
+    i = 1;
+  }
+  A = N / i;
+  B = N - A;
+  printf(" %d %d ", A, B);
+  return 0;
+  
 }
